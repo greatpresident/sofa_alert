af727f36f78ba8009537224c3f338ca57bbe7684
greatpresident <greatpresident@sina.com>
2014-05-14 23:25:34 +0800

    update to version 1.1: modify for Renren.com V7 UI

--- detect.js
+++ detect.js
@@ -3,17 +3,19 @@ function search() {
         document.getElementsByClassName("a-feed"),
         function(feed){
             var replies = feed.getElementsByClassName("feed-replies")[0]
-        if (replies != undefined && replies.childElementCount > 0) {
-            var sofa = replies.firstElementChild
-        var sofa_name = sofa.querySelector(".reply-content a").textContent
-        var lz_name = feed.querySelector("h3 a").textContent
-        return {
-            sofa: sofa_name,
-           lz: lz_name,
-           position: position(feed)
-        }; //sofa：沙发名，lz：楼主名，position：状态在页面中的绝对坐标
-        }
-        }).filter(function(n){return !!n;});
+            if (replies){
+                var sofa = null
+                if (sofa = replies.querySelector(".a-reply")) {
+                   var sofa_name = sofa.querySelector(".reply-info a").textContent
+                   var lz_name = feed.querySelector(".feed-user-info a").textContent
+                   return {
+                       sofa: sofa_name,
+                       lz: lz_name,
+                       position: position(feed)
+                   }; //sofa：沙发名，lz：楼主名，position：状态在页面中的绝对坐标
+                }
+            }
+        }).filter(function(n){return !!n;})
 }
 
 function position(feed) {
@@ -30,7 +32,7 @@ function position(feed) {
 }
 
 //----------------main----------------------
-console.log("监听器启动")
+console.log("沙发监听器启动")
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     switch (request.message) {
     case "sofas":
@@ -39,16 +41,16 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         })
         break
     case "focus":
-    //尽量保证窗口滚动后横坐标不变，除非窗口太靠右导致新鲜事左边无法显示
-    var scrollX = request.position.x < window.scrollX ? request.position.x : window.scrollX
-    window.scrollTo(scrollX, request.position.y - paddingTop)
-    break
+       //尽量保证窗口滚动后横坐标不变，除非窗口太靠右导致新鲜事左边无法显示
+       var scrollX = request.position.x < window.scrollX ? request.position.x : window.scrollX
+       window.scrollTo(scrollX, request.position.y - paddingTop)
+       break
     default:
-    sendResponse({
-        result: "不告诉你"
-    })
+       sendResponse({
+           result: "不告诉你"
+       })
     }
 })
 
 //-----------------全局变量-------------------
-var paddingTop = 20 //状态的CSS里找到的属性
+var paddingTop = 50 //状态的CSS里找到的属性
